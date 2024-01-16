ae1bfa9972201187a8eb7ae86943f6fd19014b94
Josh Wilson <josh.wilson@zbdsh.tech>
2024-01-16 22:31:40 +1100

    Overhauled celeste world to more easily handle data

--- worlds/celeste/__init__.py
+++ worlds/celeste/__init__.py
@@ -4,15 +4,22 @@ from typing import List
 
 from BaseClasses import Item, ItemClassification, MultiWorld, Tutorial
 from worlds.AutoWorld import WebWorld, World
-
-from .items import (
+from worlds.celeste.data import (
+    BaseData,
     CelesteItem,
-    CelesteItemFactory,
     CelesteItemType,
-    CelesteLocationFactory,
+    CelesteLevel,
+    CelesteLocation,
+    CelesteSide,
 )
-from .options import VictoryConditionEnum, celeste_options, get_option_value
-from .regions import CelesteRegionFactory
+from worlds.celeste.progression import BaseProgression, DefaultProgression
+
+from .options import (
+    ProgressionSystemEnum,
+    VictoryConditionEnum,
+    celeste_options,
+    get_option_value,
+)
 
 
 class CelesteWebWorld(WebWorld):
@@ -40,83 +47,55 @@ class CelesteWorld(World):
     topology_present = True
     web = CelesteWebWorld()
 
-    victory_condition: VictoryConditionEnum
-    completion_level: int
+    progression_system: BaseProgression
 
-    item_factory: CelesteItemFactory
-    location_factory: CelesteLocationFactory
-    region_factory: CelesteRegionFactory
-
-    item_name_to_id = CelesteItemFactory.get_name_to_id()
-    location_name_to_id = CelesteLocationFactory.get_name_to_id()
+    item_name_to_id = BaseData.item_name_to_id()
+    location_name_to_id = BaseData.location_name_to_id()
 
     required_client_version = (0, 4, 3)
 
     def __init__(self, multiworld: MultiWorld, player: int):
         super().__init__(multiworld, player)
         self.completion_level = 10
-        self.item_factory = CelesteItemFactory(self)
-        self.location_factory = CelesteLocationFactory()
-        self.region_factory = CelesteRegionFactory()
 
     def generate_early(self) -> None:
-        self.victory_condition = VictoryConditionEnum(
-            get_option_value(self.multiworld, self.player, "victory_condition")
-        )
-        if self.victory_condition == VictoryConditionEnum.CHAPTER_9_FAREWELL:
-            self.completion_level = 10
-        elif self.victory_condition == VictoryConditionEnum.CHAPTER_8_CORE:
-            self.completion_level = 9
-        elif self.victory_condition == VictoryConditionEnum.CHAPTER_7_SUMMIT:
-            self.completion_level = 7
+        options = {x: get_option_value(self.multiworld, self.player, x) for x in celeste_options}
 
-    def create_item(self, name: str) -> Item:
-        return self.item_factory.create_item(name)
+        if options["progression_system"] == ProgressionSystemEnum.DEFAULT_PROGRESSION.value:
+            self.progression_system = DefaultProgression(options)
+
+    def create_item(self, name: str) -> CelesteItem:
+        uuid = self.item_name_to_id[name]
+        return self.progression_system.items(self.player, self.multiworld)[uuid].copy()
 
     def create_regions(self):
-        self.region_factory.activate(self)
+        regions = self.progression_system.regions(self.player, self.multiworld)
+        self.multiworld.regions.extend(regions)
 
     def create_items(self):
-        item_table = self.item_factory.get_table(self)
+        item_table = self.progression_system.items(self.player, self.multiworld).values()
 
         for item in item_table:
-            if item.level > self.completion_level:
-                continue
-            if item.level == self.completion_level and item.side == 0 and item.item_type == CelesteItemType.COMPLETION:
-                continue
-            self.multiworld.itempool.append(deepcopy(item))
+            self.multiworld.itempool.append(item.copy())
 
         self.item_name_groups = {
             "cassettes": [item.name for item in item_table if item.item_type == CelesteItemType.CASSETTE],
-            "completions": [item.name for item in item_table if item.item_type == CelesteItemType.COMPLETION],
-            "gemhearts": [item.name for item in item_table if item.item_type == CelesteItemType.GEMHEART],
-            "strawberries": [item.name for item in item_table if item.item_type == CelesteItemType.STRAWBERRY],
+            "levels": [item.name for item in item_table if item.item_type == CelesteItemType.COMPLETION],
+            "hearts": [item.name for item in item_table if item.item_type == CelesteItemType.GEMHEART],
+            "berries": [item.name for item in item_table if item.item_type == CelesteItemType.STRAWBERRY],
         }
 
     def generate_basic(self) -> None:
-        if self.victory_condition == VictoryConditionEnum.CHAPTER_9_FAREWELL:
-            self.multiworld.completion_condition[self.player] = lambda state: state.has(
-                "Completion (Chapter 9: Farewell)", self.player
-            )
-        elif self.victory_condition == VictoryConditionEnum.CHAPTER_8_CORE:
-            self.multiworld.completion_condition[self.player] = lambda state: state.has(
-                "Completion (Chapter 8: Core A-Side)", self.player
-            )
-        elif self.victory_condition == VictoryConditionEnum.CHAPTER_7_SUMMIT:
-            self.multiworld.completion_condition[self.player] = lambda state: state.has(
-                "Completion (Chapter 7: The Summit A-Side)", self.player
-            )
-
-    def pre_fill(self):
-        item_table = self.item_factory.get_table(self)
-        for item in item_table:
-            if item.level > self.completion_level or (
-                item.level == self.completion_level and item.side == 0 and item.item_type == CelesteItemType.COMPLETION
-            ):
-                self.multiworld.get_location(item.name, self.player).place_locked_item(deepcopy(item))
+        self.multiworld.completion_condition[self.player] = lambda state: state.has(
+            self.progression_system.victory_item_name(), self.player
+        )
 
     def fill_slot_data(self):
         slot_data = {}
         for option_name in celeste_options:
-            slot_data[option_name] = get_option_value(self.multiworld, self.player, option_name)
+            option = self.progression_system.get_option(option_name)
+            if option != get_option_value(self.multiworld, self.player, option_name):
+                print(f"[WARNING] [CELESTE] Options value {option_name} overridden as {option}.")
+            slot_data[option_name] = option
+
         return slot_data
---
+++ worlds/celeste/progression.py
@@ -0,0 +1,225 @@
+# noqa # pylint: disable=missing-class-docstring, missing-function-docstring, missing-module-docstring, fixme, unnecessary-lambda-assignment
+from abc import ABC, abstractmethod
+from typing import Callable, Dict, List, Optional
+
+from BaseClasses import CollectionState, ItemClassification, MultiWorld, Region
+from worlds.celeste.data import (
+    STRAWBERRY_UUID,
+    BaseData,
+    CelesteItem,
+    CelesteItemType,
+    CelesteLevel,
+    CelesteLocation,
+    CelesteSide,
+)
+from worlds.celeste.options import VictoryConditionEnum
+
+
+class BaseProgression(ABC):
+    _options: Dict[str, int | bool]
+
+    def __init__(self, options: Dict[str, int | bool]):
+        self._options = options
+
+    def _goal_level(self) -> CelesteLevel:
+        victory_condition = self.get_option("victory_condition")
+        if victory_condition == VictoryConditionEnum.CHAPTER_9_FAREWELL.value:
+            return CelesteLevel.FAREWELL
+        elif victory_condition == VictoryConditionEnum.CHAPTER_8_CORE.value:
+            return CelesteLevel.CORE
+        elif victory_condition == VictoryConditionEnum.CHAPTER_7_SUMMIT.value:
+            return CelesteLevel.THE_SUMMIT
+
+    def _goal_side(self) -> CelesteLevel:
+        return CelesteSide.A_SIDE
+
+    def get_option(self, name: str) -> int | bool:
+        return self._options[name]
+
+    @abstractmethod
+    def victory_item_name(self) -> str:
+        return ""
+
+    @abstractmethod
+    def regions(self, player: int, multiworld: MultiWorld) -> List[Region]:
+        return []
+
+    @abstractmethod
+    def items(self, player: int, multiworld: MultiWorld) -> Dict[int, CelesteItem]:
+        return []
+
+    @abstractmethod
+    def locations(self, player: int, multiworld: MultiWorld) -> Dict[int, CelesteLocation]:
+        return []
+
+
+class DefaultProgression(BaseProgression):
+    _regions: List[Region]
+    _items: Dict[int, CelesteItem]
+    _locations: Dict[int, CelesteLocation]
+
+    @staticmethod
+    def _region_access_rule(
+        player: int, level: CelesteLevel, side: CelesteSide
+    ) -> Optional[Callable[[CollectionState], bool]]:
+        if side == 0:
+            if level == 1:
+                return None
+            return lambda state: state.has_any(
+                {
+                    BaseData.item_name(CelesteItemType.COMPLETION, level.previous(), CelesteSide.A_SIDE),
+                    BaseData.item_name(CelesteItemType.COMPLETION, level.previous(), CelesteSide.B_SIDE),
+                    BaseData.item_name(CelesteItemType.COMPLETION, level.previous(), CelesteSide.C_SIDE),
+                },
+                player,
+            )
+        elif side == 1:
+            return lambda state: state.has(
+                BaseData.item_name(CelesteItemType.CASSETTE, level, CelesteSide.A_SIDE), player
+            )
+        elif side == 2:
+            return lambda state: state.has_all(
+                {
+                    BaseData.item_name(CelesteItemType.COMPLETION, level, CelesteSide.A_SIDE),
+                    BaseData.item_name(CelesteItemType.COMPLETION, level, CelesteSide.B_SIDE),
+                },
+                player,
+            )
+        else:
+            return None
+
+    def _location_access_rule(
+        self, player: int, level: CelesteLevel, side: CelesteSide
+    ) -> Optional[Callable[[CollectionState], bool]]:
+        access_rule = lambda state: True
+
+        if level == CelesteLevel.CORE:
+            if side == CelesteSide.A_SIDE:
+                access_rule = lambda state: access_rule(state) and state.has_group("gemhearts", player, 4)
+            elif side == CelesteSide.B_SIDE:
+                access_rule = lambda state: access_rule(state) and state.has_group("gemhearts", player, 15)
+            elif side == CelesteSide.C_SIDE:
+                access_rule = lambda state: access_rule(state) and state.has_group("gemhearts", player, 23)
+
+        if level == self._goal_level() and side == self._goal_side():
+            access_rule = lambda state: (
+                access_rule
+                and state.has_group(
+                    "hearts",
+                    player,
+                    self.get_option("hearts_required"),
+                )
+                and state.has_group(
+                    "berries",
+                    player,
+                    self.get_option("berries_required"),
+                )
+                and state.has_group(
+                    "levels",
+                    player,
+                    self.get_option("levels_required"),
+                )
+                and state.has_group(
+                    "cassettes",
+                    player,
+                    self.get_option("cassettes_required"),
+                )
+            )
+
+        return access_rule
+
+    def victory_item_name(self) -> str:
+        return BaseData.item_name(CelesteItemType.COMPLETION, self._goal_level(), self._goal_side())
+
+    def regions(self, player: int, multiworld: MultiWorld) -> List[Region]:
+        if self._regions is not None:
+            return self._regions
+        self._regions = []
+
+        if self._locations is None:
+            self.locations(player, multiworld)
+
+        menu_region = Region("Menu", player, multiworld)
+        self._regions.append(menu_region)
+        map_region = Region("Map", player, multiworld)
+        self._regions.append(map_region)
+        menu_region.connect(map_region)
+
+        for level, side, name in BaseData.regions(self._goal_level(), self._goal_side()):
+            level_region = Region(name, player, multiworld)
+            self._regions.append(level_region)
+            map_region.connect(
+                level_region, f"Load {name}", DefaultProgression._region_access_rule(player, level, side)
+            )
+            native_locations = [
+                location for location in self._locations if location.level == level and location.side == side
+            ]
+            for location in native_locations:
+                level_region.locations.append(location)
+                location.parent_region = level_region
+
+        return self._regions
+
+    def items(self, player: int, multiworld: MultiWorld) -> Dict[int, CelesteItem]:
+        if self._items is not None:
+            return self._items
+        self._items = {}
+
+        item_count = {
+            CelesteItemType.CASSETTE: 0,
+            CelesteItemType.COMPLETION: 0,
+            CelesteItemType.GEMHEART: 0,
+            CelesteItemType.STRAWBERRY: 0,
+        }
+
+        for (item_type,) in BaseData.items(self._goal_level(), self._goal_side(), False):
+            item_count[item_type] += 1
+
+        self._options["berries_required"] = min(
+            self.get_option("berries_required"), item_count[CelesteItemType.STRAWBERRY]
+        )
+        self._options["cassettes_required"] = min(
+            self.get_option("cassettes_required"), item_count[CelesteItemType.CASSETTE]
+        )
+        self._options["hearts_required"] = min(self.get_option("hearts_required"), item_count[CelesteItemType.GEMHEART])
+        self._options["levels_required"] = min(
+            self.get_option("levels_required"), item_count[CelesteItemType.COMPLETION]
+        )
+
+        required_strawberries = self.get_option("berries_required")
+        strawberry_count = 0
+        for item_type, level, side, name, uuid in BaseData.items(self._goal_level(), self._goal_side()):
+            classification = ItemClassification.progression
+            if item_type == CelesteItemType.STRAWBERRY:
+                uuid = STRAWBERRY_UUID
+                if strawberry_count >= required_strawberries:
+                    classification = ItemClassification.filler
+                strawberry_count += 1
+
+            item = CelesteItem(
+                name=name,
+                classification=classification,
+                code=uuid,
+                player=player,
+                item_type=item_type,
+                level=level,
+                side=side,
+            )
+            self._items[uuid] = item
+
+        return self._items
+
+    def locations(self, player: int, multiworld: MultiWorld) -> Dict[int, CelesteLocation]:
+        if self._locations is not None:
+            return self._locations
+        self._locations = {}
+
+        if self._items is None:
+            self.items(player, multiworld)
+
+        for level, side, name, uuid in BaseData.locations(self._goal_level(), self._goal_side()):
+            location = CelesteLocation(player, level, side, name, uuid)
+            location.access_rule = self._location_access_rule(player, level, side)
+            self._locations[uuid] = location
+
+        return self._locations
